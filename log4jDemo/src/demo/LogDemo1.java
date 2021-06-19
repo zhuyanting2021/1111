34b5c06ff0da46d6e95bd23e71e6793c322fbd6c
zhuyanting <531266923@qq.com>
2021-06-19 10:48:06 +0800

    first commit

---
+++ log4jDemo/src/demo/LogDemo1.java
@@ -0,0 +1,30 @@
+package demo;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * @author zhuyanting
+ */
+public class LogDemo1 {
+	public static void main(String[] args) throws InterruptedException {
+		Logger logger = Logger.getLogger(LogDemo1.class);
+		/*
+		 * 设置默认级别 BasicConfigurator.configure();
+		 * 设置日志输出级别
+		 * logger.setLevel(Level.DEBUG); logger.trace("跟踪信息");
+		 * logger.debug("调试信息"); logger.info("输出信息"); Thread.sleep(1000);
+		 * logger.warn("警告信息"); logger.error("错误信息"); logger.fatal("致命信息");
+		 */
+
+		PropertyConfigurator.configure("E:\\eclipsePratice\\log4jDemo\\src\\demo\\log4j.properties");
+		for (int i = 0; i < 100; i++) {
+			logger.trace("跟踪信息");
+			logger.debug("调试信息");
+			logger.info("输出信息");
+			logger.warn("警告信息");
+			logger.error("错误信息");
+			logger.fatal("致命信息");
+		}
+	}
+}
