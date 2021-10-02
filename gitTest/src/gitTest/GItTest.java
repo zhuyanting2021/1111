22fc35aee27c0a143d0f57509be4e318bdb6d035
zhuyanting <531266923@qq.com>
2021-10-03 04:36:43 +0800

    foreach

--- gitTest/src/gitTest/GItTest.java
+++ gitTest/src/gitTest/GItTest.java
@@ -1,11 +1,22 @@
 package gitTest;
 
+
+import java.util.HashMap;
+
 public class GItTest {
-	public static void main(String[] args) {
-		System.out.println("第一次提交 git");
-		System.out.println("第二次提交了");
-		System.out.println("sysconized");
-		System.out.println("hahaha");
-	}
+    public static void main(String[] args) {
+        System.out.println("第一次提交 git");
+        System.out.println("第二次提交了");
+        System.out.println("sysconized");
+        System.out.println("hahaha");
+
+        HashMap<String, String> map1 = new HashMap<>();
+        map1.put("111", "ddd");
+        for (String key : map1.keySet()) {
+
+            System.out.println(map1.get(key));
+
+        }
+    }
 
 }
