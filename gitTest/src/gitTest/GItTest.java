386487693edb07bc3b286b70f065c157b8931170
zhuyanting <531266923@qq.com>
2021-12-03 15:10:27 +0800

    1234

--- gitTest/src/gitTest/GItTest.java
+++ gitTest/src/gitTest/GItTest.java
@@ -7,7 +7,7 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("第一次提交 git");
         System.out.println("第二次提交了");
-        System.out.println("sysconized");
+        System.out.println("sysconized1234");
         System.out.println("hahaha");
 
         HashMap<String, String> map1 = new HashMap<>();
